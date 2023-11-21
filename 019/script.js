0d19095a2024cd2b472ca089e309174b2d642985
OdenthalPetr <145441108+OdenthalPetr@users.noreply.github.com>
2023-11-21 10:50:42 +0100

    Add files via upload

---
+++ 019/script.js
@@ -0,0 +1,16 @@
+function moudro() {
+    var item = citaty[Math.floor(Math.random()*citaty.length)]
+    document.getElementById("text").innerHTML = item;
+}
+var citaty = ["Odmítáme ty co nás milují, a milujeme ty co nás odmítají",
+"Představte si to ticho, kdyby lidé říkali jen to, co vědí.",
+"Čtenář prožije tisíc životů, než zemře. Člověk, jenž nikdy nečte, prožije jen jeden.",
+"Mějte dobrou náladu. Dobrá nálada vaše problémy sice nevyřeší, ale naštve tolik lidí kolem, že stojí za to si ji užít",
+"Správně vidíme jen srdcem. Co je důležité je očím neviditelné",
+"Žít, to je nejvzácnější věc na světě, neboť většina lidí jenom existuje.",
+"Pouze dvě věci jsou nekonečné. Vesmír a lidská hloupost. U té první si tím však nejsem tak jist.",
+"Svět patří těm, co se neposerou.",
+"Mnozí z těch, co žijí, by zasluhovali smrt. A mnozí z těch, co zemřeli, by si zasloužili žít.",
+" Na světě nejsou nejkrásnější věci, ale okamžiky",
+" Peklo je prázdné, ďáblové jsou mezi námi.",]
+
